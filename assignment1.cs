99ee18843214490809badb12bdab44b4114178e6
Snehalathavr <128053582+Snehalathavr@users.noreply.github.com>
2023-03-26 22:55:52 -0700

    Add files via upload

---
+++ assignment1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class MyClass{
+
+    public static void Main(){
+        var name="Snehalatha";
+        Console.WriteLine($"hello world this is {name}");
+        Console.ReadKey();
+    }
+}
